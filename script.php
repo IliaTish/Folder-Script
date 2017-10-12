88377f693464932b66fa0bcf8761da42eeb3a6cb
= <pintos.ilya@ya.ru>
2017-10-12 11:26:37 +0300

    remove isDot method

--- script.php
+++ script.php
@@ -7,13 +7,6 @@
 		return defined('STDIN') || (substr(PHP_SAPI, 0, 3) == 'cgi' && getenv('TERM'));
 	}
 
-	function isDot(string $folder): bool {
-		if ($folder === "." || $folder === "..") {
-			return true;
-		}
-		return false;
-	}
-
 	function printToConsole(string $directory, int $space = 0): void {
 		echo str_repeat("\t", $space).$directory."\n";
 	}
@@ -33,17 +26,15 @@
 	}
 
 	function scanDirectories(string $current_dir, int $space = 1): void {
-		$folder_array = scandir($current_dir);
+		$folder_array = array_diff(scandir($current_dir),array(".",".."));
 		foreach ($folder_array as $key => $folder) {
-			if (!isDot($folder)) {
-				$new_dir = $current_dir."/".$folder;
-				if (is_dir($new_dir) && !is_link($new_dir)) {
-					printDirectory($new_dir, $space);
-					scanDirectories($new_dir, $space+1);
-				}
-				else {
-					printDirectory($new_dir, $space);
-				}
+			$new_dir = $current_dir."/".$folder;
+			if (is_dir($new_dir) && !is_link($new_dir)) {
+				printDirectory($new_dir, $space);
+				scanDirectories($new_dir, $space+1);
+			}
+			else {
+				printDirectory($new_dir, $space);
 			}
 		}
 	}
